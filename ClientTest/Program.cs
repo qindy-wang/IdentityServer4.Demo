3bb61c84a9a0a49aef088f49ab6defa994617db9
qdwang <qindy020307@163.com>
2021-03-19 15:32:16 +0800

    [IdentityServer4]add ClientCredential GrantType project

---
+++ ClientTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //discovery endpoint from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+            //Get access token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api"
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.Write(tokenResponse.Json);
+
+            //call API
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = await apiClient.GetAsync("https://localhost:6001/api/IdentityServer");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+                return;
+            }
+        }
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityModel.OidcConstants;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("api1","My API")
+            };
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    AllowedGrantTypes= { OidcConstants.GrantTypes.ClientCredentials },
+                    ClientId="client",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".ToSha256())
+                    },
+                    AllowedScopes  ={"api1"}
+                }
+            };
+    }
+}
